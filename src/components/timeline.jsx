d0069e12ce63f7ecf4aafae794c3c98ad234996a
Sabbat10 <sabbatlumpatshia99@gmail.com>
2023-11-22 12:09:50 +0100

    add profil and input

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import ProfilPhoto from '../images/profile-photo.png';
 
 function Timeline() {
-
+  return (
+    <div className="tweet-editor">
+     <img src={ProfilPhoto} alt="" />
+      <div>
+        <input className="tweet-editor-input" type="text" placeholder="What's happing ?"/>
+      </div>
+    </div>
+  );
 }
 
 export default Timeline;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Timeline from '../components/timeline';
 
 function Home() {
   return (
     <main className="timeline">
       <Header/ >
+      <Timeline/ >
     </main>
   );
 }
