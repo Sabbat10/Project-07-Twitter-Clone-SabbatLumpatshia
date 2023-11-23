86c46326f6e5c7c480839c54bd553b44f6ce6ca8
Sabbat10 <sabbatlumpatshia99@gmail.com>
2023-11-23 09:34:26 +0100

    end of conponet

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -1,12 +1,31 @@
 import React from "react";
+// Importation des images svg
 import ProfilPhoto from '../images/profile-photo.png';
+import Poll from '../images/Poll.svg';
+import Gif from '../images/Gif.svg';
+import Media from '../images/Media.svg';
+import Schedule from '../images/Schedule.svg';
 
+// components Timeline
 function Timeline() {
   return (
     <div className="tweet-editor">
-     <img src={ProfilPhoto} alt="" />
-      <div>
+     <img className="avatar" src={ProfilPhoto} alt="" />
+      <div className="tweet-editor-form">
         <input className="tweet-editor-input" type="text" placeholder="What's happing ?"/>
+        
+        {/* Classe 21em */}
+        <div className="tweet-editor-buttons">
+          {/* icones des actions  */}
+          <div className="tweet-editor-actions">
+            <img src={Media} alt="" />
+            <img src={Gif} alt="" />
+            <img  src={Poll} alt="" />
+            <img  src={Schedule} alt="" />
+          </div>
+          {/* button tweet */}
+          <button className="button">Tweet</button>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/tweet.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import TweetProfilPhoto from '../images/tweet-profile-photo.png';
+import Coeur from '../images/React';
+import Reply from '../images/Reply.svg';
+import Retweet from '../images/Retweet.svg';
+import TweetReplies from '../images/Tweet-Replies.svg';
