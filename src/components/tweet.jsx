b2e34cb350068e2ff2bd3163ea0e5db555ddd657
Sabbat10 <sabbatlumpatshia99@gmail.com>
2023-11-23 13:32:01 +0100

    add to componnet twee and object data

--- src/components/tweet.jsx
+++ src/components/tweet.jsx
@@ -6,63 +6,24 @@ import react from '../images/React.svg';
 import Reply from '../images/Reply.svg';
 import Retweet from '../images/Retweet.svg';
 import TweetReplies from '../images/Tweet-Replies.svg';
+// import data  from '../data'
+import tweetImage from '../images/tweet-image.png';
 
-const Tweet = [
-  {
-    name: "CNN",
-    content:
-      'President Joe Biden touted a new agreement reached with the European Union to ease Trump-era tariffs on aluminum and steel as a "major breakthrough" that would serve to both strengthen the US steel industry and combat the global climate crisis.',
-    comments: 57,
-    retweets: 144,
-    likes: 184,
-    sent: "7m",
-    author: "@CNN",
-    profileUser: TweetProfilPhoto,
-  },
-  {
-    name: "New York Times",
-    content:
-      "Gardening boomed during the pandemic. Six Black writers share how it has helped them re-establish, and reimagine, a connection to cultivation and the land",
-    Img: secondTweetImage,
-    comments: 19,
-    retweets: 48,
-    likes: 482,
-    sent: "Oct 29",
-    author: "@nytimes",
-    profileUser: newYorkTime,
-  },
-  {
-    name: "Twitter",
-    content: "BIG NEWS lol jk still Twitter",
-    comments: "6.8K",
-    retweets: "36.6K",
-    likes: "267.1K",
-    sent: "Oct 20",
-    author: "@Twitter",
-    profileUser: roundTwitter,
-  },
-  {
-    name: "Twitter",
-    content: "Hello literally everyone",
-    comments: "18.7K",
-    retweets: "785.4K",
-    likes: "3.3M",
-    sent: "Oct 15",
-    author: "@Twitter",
-    profileUser: roundTwitter,
-  },
-  {
-    name: "Twitter",
-    content:
-      "Hello literally everyone",
-      Img: secondTweetImage,
-    comments: 19,
-    retweets: 48,
-    likes: 482,
-    sent: "Oct 17",
-    author: "@Twitter",
-    profileUser: roundTwitter,
-  },
-];
-
+function Tweet(){
+    return(
+   
+            <div className="tweet-content">
+                <img className="tweet-avatar" src={newYorkTime} alt="" />
+                
+                <div className="tweet-body">
+                    <b>The New York Time</b>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis repudiandae fugiat porro pariatur quo eveniet aspernatur soluta neque enim provident? Voluptates, dolores. Quasi, praesentium rerum perferendis reiciendis placeat ex autem!
+                    </p>
+                    <img src={tweetImage} alt="" />
+                </div>
+            </div>
+      
+    )
+}
 export default Tweet;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import Timeline from '../components/timeline';
-// import Tweet from '../components/tweet';
+import Tweet from '../components/tweet';
 
 function Home() {
   return (
     <main className="timeline">
       <Header/ >
       <Timeline/ >
-      {/* <Tweet/ >    */}
+      <Tweet/ >   
     </main>
   );
 }
