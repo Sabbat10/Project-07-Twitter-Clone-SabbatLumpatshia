cd4b90797ff9db6dc75d16ab8b8f98bca0ef7957
sabbat10 <sabbatlumpatshia99@gmail.com>
2023-12-06 14:47:12 +0100

    project improvement

--- src/components/tweet.jsx
+++ src/components/tweet.jsx
@@ -1,47 +1,123 @@
 import React from "react";
-import TweetAction from "./tweetAction";
-import data from '../data';
+import tweetImage from '../images/tweet-image.png';
+import tweetProfile from '../images/tweet-profile-photo.png';
+import roundTwitter from '../images/round-twitter-logo.svg';
+import newYorkTime from '../images/newYorkTime.svg'
 
-function Tweet () {
+import reply from '../images/Reply.svg'
+import retweet from '../images/Retweet.svg'
+import love from '../images/React.svg'
+import tweetReplie from '../images/Tweet-Replies.svg'
+
+
+function Tweet (props) {
     return (
         <div className="tweets">
-            {data.map((copyData, index) => {
-                return (
-                    <div className="tweet" key={index}>
-                        <img src={copyData.profil} className="tweet-avatar" alt="" />
+            {/* Premier tweet */}
+            <div className="tweet">
+                <div className="tweet-avatar">
+                    <span><img src={tweetProfile} alt="" /></span>
+                </div>
 
-                        <div className="tweet-content">
-                            <div className="tweet-body">
-                                <div className="tweet-title">
-                                    <span className="tweet-title-author">
-                                        {copyData.name}
-                                    </span>
-                                    <span className="tweet-title-details">
-                                        {copyData.author}
-                                    </span>
-                                    <span>{"."}</span>
-                                    <span>{copyData.sent}</span>
-                                </div>
-                                <p className="tweet-text">
-                                    {copyData.content   }
-                                </p>
-                                <div className="tweet-image">
-                                    {copyData.Images     && <img src={copyData.Images}/>}
-                                </div>
-                            </div>
-                            <TweetAction
-                                comments={copyData.comments}
-                                retweets={copyData.retweets}
-                                likes={copyData.likes}
-                            />
+                <div className="tweet-content">
+                    <div className="tweet-body">
+                        <span className="tweet-title"><b>CNN</b> @CNN . 7m</span>
+                        <span className="tweet-text"><p>President Joe Biden touted a new agreement reached with the European Union to ease Trump-era tariffs on aluminum and steel as a "major breakthrough" that would serve to both strengthen the US steel industry and combat the global climate crisis.</p></span>
+
+                        <div className="tweet-actions">
+                            <div className="tweet-action"><img  src={reply} alt="" />235k</div>
+                            <div className="tweet-action"><img  src={retweet} alt="" />235k</div>
+                            <div className="tweet-action"><img  src={love} alt="" />235k</div>
+                            <div className="tweet-action"><img  src={tweetReplie} alt="" />235k</div>
                         </div>
                     </div>
-                )
-            }
-            )
-            }
+                </div>
+            </div>
+        {/* Deuxième tweet */}
+            <div className="tweet">
+                <div className="tweet-avatar">
+                    <span><img src={newYorkTime} alt="" /></span>
+                </div>
+
+                <div className="tweet-content">
+                    <div className="tweet-body">
+                        <span className="tweet-title"><b>New York Times</b> @nytimes . 2h</span>
+                        <span className="tweet-text"><p>Gardening boomed during the pandemic. Six Black writers share how it has helped them re-establish, and reimagine, a connection to cultivation and the land</p></span>
+                        <div className="tweet-image">
+                            <img src={tweetImage} alt="" />
+                        </div>
+
+                        <div className="tweet-actions">
+                            <div className="tweet-action"><img  src={reply} alt="" />235k</div>
+                            <div className="tweet-action"><img  src={retweet} alt="" />235k</div>
+                            <div className="tweet-action"><img  src={love} alt="" />235k</div>
+                            <div className="tweet-action"><img  src={tweetReplie} alt="" />235k</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Troisième tweet */}
+
+            <div className="tweet">
+                <div className="tweet-avatar">
+                    <span><img src={roundTwitter} alt="" /></span>
+                </div>
+
+                <div className="tweet-content">
+                    <div className="tweet-body">
+                        <span className="tweet-title"><b>Twitter</b> @Twitter . Oct 29</span>
+                        <span className="tweet-text"><p>BIG NEWS lol jk still Twitter</p></span>
+
+                        <div className="tweet-actions">
+                            <div className="tweet-action"><img  src={reply} alt="" />235k</div>
+                            <div className="tweet-action"><img  src={retweet} alt="" />235k</div>
+                            <div className="tweet-action"><img  src={love} alt="" />235k</div>
+                            <div className="tweet-action"><img  src={tweetReplie} alt="" />235k</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Quatrième tweet */}
+
+            <div className="tweet">
+                <div className="tweet-avatar">
+                    <span><img src={roundTwitter} alt="" /></span>
+                </div>
+
+                <div className="tweet-content">
+                    <div className="tweet-body">
+                        <span className="tweet-title"><b>Twitter</b> @Twitter . Oct 4</span>
+                        <span className="tweet-text"><p>Hello literally everyone</p></span>
+
+                        <div className="tweet-actions">
+                            <div className="tweet-action"><img  src={reply} alt="" />235k</div>
+                            <div className="tweet-action"><img  src={retweet} alt="" />235k</div>
+                            <div className="tweet-action"><img  src={love} alt="" />235k</div>
+                            <div className="tweet-action"><img  src={tweetReplie} alt="" />235k</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Cinquième tweet */}
+
+            <div className="tweet">
+                <div className="tweet-avatar">
+                    <span><img src={roundTwitter} alt="" /></span>
+                </div>
+
+                <div className="tweet-content">
+                    <div className="tweet-body">
+                        <span className="tweet-title"><b>Twitter</b> @Twitter . Oct 4</span>
+                        <span className="tweet-text"><p>Hello literally everyone</p></span>
+
+                    </div>
+                </div>
+            </div>  
         </div>
-    )
+    );
 }
 
 export default Tweet;
